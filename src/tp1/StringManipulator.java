16ae01de13a02145db786b6e452b0aed45247b9c
Mouna-Chtourou <83994300+Mouna-Chtourou@users.noreply.github.com>
2021-10-28 16:04:13 +0100

    Update StringManipulator.java

--- src/tp1/StringManipulator.java
+++ src/tp1/StringManipulator.java
@@ -9,28 +9,69 @@ public class StringManipulator {
     }
 
     public void reverse(){
-        //do your stuff
-        System.out.println("reverse : ");
+           String revr="";
+            for(int i = s.length() - 1; i >= 0; i--)
+            {
+                revr = revr + s.charAt(i);
+            }
+            System.out.println("reverse : "+revr);
     }
 
     public void isPalindrome(){
-        //do your stuff
-        System.out.println("isPalindrome : ");
+       boolean palindrome=true;
+        for(int i=0,j=s.length()-1;i<=j;i++,j--)
+        {
+            if (s.charAt(i)!=s.charAt(j))
+            {
+                palindrome=false;
+                break;
+            }
+        }
+        System.out.println("isPalindrome : "+palindrome);
     }
 
     public void toUpperCase(){
-        //do your stuff
-        System.out.println("uppercase : ");
+      String upper="";
+        for(int i=0;i<s.length();i++){
+            int ascii=s.charAt(i);
+            if(ascii>90){
+                ascii=ascii-32;
+                char l=(char)ascii;
+                upper=upper+l;}
+            else upper=upper+(char)ascii;
+
+        }
+        System.out.println("uppercase : "+upper);
     }
 
     public void toLowerCase(){
-        //do your stuff
-        System.out.println("lowercase : ");
+      String lower="";
+            for(int i=0;i<s.length();i++){
+                int ascii=s.charAt(i);
+                if(ascii<=90){
+                    ascii=ascii+32;
+                    char l=(char)ascii;
+                    lower=lower+l;}
+                else lower=lower+(char)ascii;
+            }
+            System.out.println("lowercase : "+lower);
     }
 
     public void getVowelNumber(){
-        //do your stuff
-        System.out.println("vowelNumber : ");
+       String Vowel ="oieauyOIEAUY";
+            int nbv=0;
+            for (int i=0;i<s.length();i++)
+            {
+                for (int j=0;j< Vowel.length();j++)
+                {
+                    if (s.charAt(i)== Vowel.charAt(j))
+                    {
+                        nbv++;
+                        break;
+                    }
+                }
+            }
+            System.out.println("vowelNumber : "+nbv);
     }
 
     public static void main(String[] args) {
