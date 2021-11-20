f28bf7565808bd8930a4db0c7e744b85e6180ee1
Mouna-Chtourou <83994300+Mouna-Chtourou@users.noreply.github.com>
2021-11-21 00:10:11 +0100

    Update Triangle.java

--- src/tp1/shapes/Triangle.java
+++ src/tp1/shapes/Triangle.java
@@ -2,13 +2,22 @@ package tp1.shapes;
 
 public class Triangle {
     //attributes
+    public int side1;
+    public int side2;
+    public int side3;
+    public int hauteur;
 
     //methods
     public void perimeter(){
-        System.out.println("Perimeter : ");
+        int p=0;
+        p=side1+side2+side3;
+        System.out.println("Perimeter : "+p);
     }
 
     public void surface(){
-        System.out.println("Perimeter : ");
+        int s=0;
+        s=(side1*hauteur)/2;
+        System.out.println("Surface : "+s);
     }
 }
+
